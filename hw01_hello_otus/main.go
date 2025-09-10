e5166e7f3489f5f3255661fec83f18a12bb9cac4
Nikita <nikita@MacBook-Air-Nikita.local>
2025-09-10 20:39:05 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	phrase := "Hello, OTUS!"
+    reversed := reverse.String(phrase)
+    fmt.Println(reversed)
 }
